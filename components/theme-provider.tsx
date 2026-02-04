e2e1783163baba9852eda8a4ddc37c4eae101922
Brigimar <160976236+brigimar@users.noreply.github.com>
2026-02-04 17:16:05 -0300

    Update theme-provider.tsx

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -4,15 +4,7 @@ import * as React from "react";
 import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return (
-    <NextThemesProvider
-      {...props}
-      attribute="class"      // importante para Tailwind dark/light
-      defaultTheme="system"  // opcional: 'light', 'dark' o 'system'
-      enableSystem={true}    // permite que siga el sistema
-    >
-      {children}
-    </NextThemesProvider>
-  );
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
+
 
